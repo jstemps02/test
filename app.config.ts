f2dea259b616e690c9ec22e374fa5c29e5704b7b
jstemps02 <jstemps02@gmail.com>
2023-03-24 13:57:45 -0500

    doin that work

--- app.config.ts
+++ app.config.ts
@@ -17,6 +17,10 @@ export default defineAppConfig({
       }
     },
     footer: {
+      credits: {
+        enabled: false, // possible value are : true | false
+        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+      },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
